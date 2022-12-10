10ee2f3b1bb5b61109fd210c245bfa9e4217af4b
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2022-12-09 23:24:38 -0500

    v1.0.0 for `ESP8266 + LwIP W5500`
    #### Releases v1.0.0
    
    1. Initial coding to port [ESPAsync_WiFiManager](https://github.com/khoih-prog/ESPAsync_WiFiManager) to ESP8266 boards using `LwIP W5500 Ethernet`.
    2. Use `allman astyle`

---
+++ src/AsyncESP8266_W5500_Manager.h
@@ -0,0 +1,33 @@
+/****************************************************************************************************************************
+  AsyncESP8266_W5500_Manager.h
+
+  For Ethernet shields using ESP8266_W5500 (ESP8266 + LwIP W5500)
+
+  WebServer_ESP8266_W5500 is a library for the ESP8266 with Ethernet W5500 to run WebServer
+
+  Modified from
+  1. Tzapu               (https://github.com/tzapu/WiFiManager)
+  2. Ken Taylor          (https://github.com/kentaylor)
+  3. Alan Steremberg     (https://github.com/alanswx/ESPAsyncWiFiManager)
+  4. Khoi Hoang          (https://github.com/khoih-prog/ESPAsync_WiFiManager)
+
+  Built by Khoi Hoang https://github.com/khoih-prog/AsyncESP8266_W5500_Manager
+  Licensed under MIT license
+
+  Version: 1.0.0
+
+  Version Modified By  Date      Comments
+  ------- -----------  ---------- -----------
+  1.0.0   K Hoang     10/12/2022 Initial coding for ESP8266_W5500
+ *****************************************************************************************************************************/
+
+#pragma once
+
+#ifndef AsyncESP8266_W5500_Manager_h
+#define AsyncESP8266_W5500_Manager_h
+
+#include <AsyncESP8266_W5500_Manager.hpp>              //https://github.com/khoih-prog/AsyncESP8266_W5500_Manager
+#include <AsyncESP8266_W5500_Manager_Impl.h>           //https://github.com/khoih-prog/AsyncESP8266_W5500_Manager
+
+#endif    // AsyncESP8266_W5500_Manager_h
+
